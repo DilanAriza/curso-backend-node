9a0a01940adbafceb7f8cd6f04e9e085e2468778
Dilan Ariza <dilansantiagoariza@gmail.com>
2020-10-10 16:45:19 -0500

    administrar directorios con nodejs

---
+++ arquitectura-node/so-archivos/os.js
@@ -0,0 +1,20 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+const os = require('os');
+
+//CPUS
+// console.log("CPU info", os.cpus());
+
+//IP
+// console.log("IP address", os.networkInterfaces().lo.map(i => i.address));
+
+//Memory
+// console.log("Free memory", os.freemem());
+
+//SO
+// console.log("Type", os.type())
+
+//Version
+// console.log("SP version", os.release());
+
+//User info
+// console.log("Urs info", os.userInfo())
